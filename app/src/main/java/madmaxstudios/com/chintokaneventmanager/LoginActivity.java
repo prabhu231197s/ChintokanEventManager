02171113901067030e10d3395dd2379678f82344
DESKTOP-PKFT7F6\Prabhu Sivanandam <prabhu231197@gmail.com>
2017-06-14 15:23:37 +0530

    Initial commit

---
+++ app/src/main/java/madmaxstudios/com/chintokaneventmanager/LoginActivity.java
@@ -0,0 +1,17 @@
+package madmaxstudios.com.chintokaneventmanager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Prabhu Sivanandam on 14-Jun-17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
